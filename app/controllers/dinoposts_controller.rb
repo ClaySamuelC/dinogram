1e0d8e0f9599c82449cddb7fa6f1d016651ae7a6
ClaySamuelC <claysamuelc@gmail.com>
2022-11-06 21:22:54 -0800

    Add view to see individual posts.

--- app/controllers/dinoposts_controller.rb
+++ app/controllers/dinoposts_controller.rb
@@ -2,4 +2,8 @@ class DinopostsController < ApplicationController
   def index
     @dinoposts = Dinopost.all
   end
+
+  def show
+    @dinopost = Dinopost.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "dinoposts#index"
 
-  get '/dinoposts', to: "dinoposts#index"
+  resources :dinoposts
 end
