3e0eb683f90bffebfdc675260b93dc6738bfc248
ClaySamuelC <claysamuelc@gmail.com>
2022-11-06 19:09:04 -0800

    Add default route to dinopost#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'dinopost/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "dinopost#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/dinopost', to: "dinopost#index"
 end
