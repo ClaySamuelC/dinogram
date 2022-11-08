d858f9415e8005e44f3337d1f16579acac6f11e1
ClaySamuelC <claysamuelc@gmail.com>
2022-11-08 13:34:40 -0800

    Fix issue with comment not being created properly.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def create
+    @dinopost = Dinopost.find(params[:dinopost_id])
+    @comment = @dinopost.comments.create(comment_params)
+    
+    redirect_to dinopost_path(@dinopost)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
